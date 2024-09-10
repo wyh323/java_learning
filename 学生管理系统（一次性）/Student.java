f29b061760e18298119fb8f722d57b248237c6eb
wuyunhao <2072887718@qq.com>
2024-09-10 22:42:17 +0800

    class student

---
+++ 学生管理系统（一次性）/Student.java
@@ -0,0 +1,36 @@
+class Student{
+    String name;
+    int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    /**
+     * 获取
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     * @return age
+     */
+    public int getAge() {
+        return age;
+    }
+}
